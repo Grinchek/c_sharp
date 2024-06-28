a2e6d148ea6d1ad3bb1e4c5f6074b58587fc4090
Grinchek <warning686@gmail.com>
2024-06-28 17:52:13 +0300

    Interfaces classwork

---
+++ c_sharp/08_interfaces/Array.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_interfaces
+{
+    internal class My_Array : IOutput, ISort, IMath
+    {
+        Random random=new Random();
+        int[] array;
+        public My_Array(int size)
+        {
+            array = new int[size];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(100);
+            }
+        }
+        public float Avg()
+        {
+            return (float)array.Average();
+        }
+
+        public int Max()
+        {
+            return array.Max();
+        }
+
+        public int Min()
+        {
+            return array.Min();
+        }
+
+        public void My_Show()
+        {
+            foreach (var elem in array) 
+            {
+                Console.Write($"[{elem}] ");
+            }
+            Console.WriteLine();
+        }
+
+        public void My_Show(string info)
+        {
+            Console.WriteLine(info);
+            foreach (var elem in array)
+            {
+                Console.Write($"[{elem}] ");
+            }
+            Console.WriteLine();
+        }
+
+        public bool Search(int valueToSearch)
+        {
+            bool result = false;
+            foreach(int value in array)
+            {
+                if (value == valueToSearch)
+                {
+                    result= true;
+                }
+                else
+                {
+                    result= false;
+                }
+            }
+            return result;
+        }
+
+        public void SortAsc()
+        {
+            Array.Sort(array);
+        }
+
+        public void SortByParam(bool isAsc)
+        {
+            if (isAsc == true)
+            {
+                SortAsc();
+            }
+            else
+            {
+                SortDesc();
+            }
+        }
+
+        public void SortDesc()
+        {
+            Array.Sort(array);
+            Array.Reverse(array);
+        }
+
+    }
+}
---
+++ c_sharp/08_interfaces/Program.cs
@@ -0,0 +1,25 @@
+﻿using _08_interfaces;
+
+namespace _c_sharp_
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            My_Array array = new My_Array(5);
+            array.My_Show();
+            Console.WriteLine("====================");
+            array.SortAsc();
+            array.My_Show();
+            Console.WriteLine("====================");
+            array.SortDesc();
+            array.My_Show("My message");
+            array.SortByParam(true);
+            Console.WriteLine("====================");
+            array.My_Show();
+            Console.WriteLine("====================");
+            Console.WriteLine(array.Search(22));
+
+        }
+    }
+}
