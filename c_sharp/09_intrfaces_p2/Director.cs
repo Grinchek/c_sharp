bffe5481959b6315b3fad2b4b64f49298a1650e6
Grinchek <warning686@gmail.com>
2024-07-01 17:52:14 +0300

    Classwork

---
+++ c_sharp/09_intrfaces_p2/Director.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_intrfaces_p2
+{
+    internal class Director:ICloneable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Director() { }
+        public Director(string name,string sourname) 
+        {
+            FirstName = name;
+            LastName = sourname;
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ c_sharp/09_intrfaces_p2/Movie.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_intrfaces_p2
+{
+    enum Genre
+    {
+        Comedy,
+        Horror,
+        Adventure,
+        Drama
+    }
+    internal class Movie:ICloneable, IComparable<Movie>
+    {
+        public string Title { get; set; }
+        public Director Director { get; set; }
+        public Genre Genre { get; set; }
+        public string Country { get; set; }
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = (value<2024&&value>1894)?value:throw   new Exception(); }
+        }
+        public Movie() { }
+        public Movie(string title, string name,string sourname, Genre genre, string country, int year)
+        {
+            Title = title;
+            Director = new Director(name, sourname);
+            this.Genre = genre;
+            Country = country;
+            Year = year;
+        }
+        public override string ToString()
+        {
+            return $"Title: {Title}\nDirector: {Director}" +
+                $"\n{this.Genre}\n{Country}\n{Year} y";
+        }
+
+        public object Clone()
+        {
+            
+            Movie copy= (Movie)this.MemberwiseClone();
+            copy.Director=(Director)this.Director.Clone();
+
+            return copy;
+        }
+
+        public int CompareTo(Movie? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
