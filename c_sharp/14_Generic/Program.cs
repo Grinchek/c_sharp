7d9d9d3e9a6259eb5a3bafc5a4eb58fc119a757d
Grinchek <warning686@gmail.com>
2024-07-08 17:37:02 +0300

    Generics type

---
+++ c_sharp/14_Generic/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _c_sharp_
+{
+
+    class Program
+    {
+        public static void MaximumNumber<T>(T a,T b, T c) 
+        {
+            T max;
+            if (a > b || a > c)
+            {
+                max = a;
+            }
+            Console.WriteLine($"Maximum number is: {max}");
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
