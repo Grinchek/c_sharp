b673dcded30d1c25ad5ad26c49591f136167f597
Grinchek <warning686@gmail.com>
2024-07-12 17:46:10 +0300

    Regular expresion

--- c_sharp/14_Generic/obj/Debug/net6.0/14_Generic.AssemblyInfo.cs
+++ c_sharp/14_Generic/obj/Debug/net6.0/14_Generic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("14_Generic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d31f1d22ebe1baa5bf9c5066166b17ca908b60b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("14_Generic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("14_Generic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ c_sharp/16_RegularExpresion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _c_sharp_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
