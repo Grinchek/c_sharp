7c34854494422956cd5e033aaa88ae00f913d612
Grinchik <warning686@gmail.com>
2024-07-12 13:29:09 +0300

    Generics homework is almost done

--- c_sharp/14_Generic/Program.cs
+++ c_sharp/14_Generic/Program.cs
@@ -1,19 +1,44 @@
-﻿namespace _c_sharp_
+﻿using _14_Generic;
+
+namespace _c_sharp_
 {
 
     class Program
     {
-        public static void MaximumNumber<T>(T a,T b, T c) 
+        public static void MaximumNumber<T>(T[] arr)
         {
-            T max;
-            if (a > b || a > c)
-            {
-                max = a;
-            }
-            Console.WriteLine($"Maximum number is: {max}");
+
+            Console.WriteLine($"Maximum number is: {arr.Max()}");
         }
+        public static void MinimumNumber<T>(T[] arr)
+        {
+
+            Console.WriteLine($"Minimum number is: {arr.Min()}");
+        }
+        public static T Sum<T>(T[] array) where T : struct
+        {
+            dynamic sum = default(T);
+            foreach (var item in array)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+        
+
         static void Main(string[] args)
         {
+            int[] intArr = { 1, 2, 3, 4, 5, 6, };
+            MyStack<int> myStack=new MyStack<int>(intArr);
+            myStack.Push(10);
+            myStack.Display();
+            myStack.Pop();
+            myStack.Pop();
+            myStack.Display();
+
+
+
+
 
         }
     }
---
+++ c_sharp/14_Generic/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Generic
+{
+    public class MyStack<T>
+    {
+        public T[] array;
+        public MyStack()
+        {
+
+        }
+        public MyStack(T[] array)
+        {
+            this.array = array;
+        }
+
+        public void Pop()
+        {
+            T[] arr = new T[array.Length - 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = array[i+1];
+            }
+            array = arr;
+        }
+        public void Push(T elem)
+        {
+            T[] arr = new T[array.Length + 1];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                arr[i] = array[i - 1];
+            }
+            arr[0] = elem;
+            array = arr;
+        }
+        public T Peek()
+        {
+            return array[0];
+        }
+        public int Count()
+        { return array.Length; }
+        public void Display()
+        {
+            foreach (var item in array)
+            {
+                Console.Write($"[{item}]");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
